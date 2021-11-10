204974c426fe0e3c1c01afbb0d2e31e5835ebcd1
Adem <adem_said@hotmail.com>
2021-11-10 11:19:56 -0500

    Initial commit for completion of fed-test
    Made changes on local machine

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,19 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}}<span class="reports_info"> ({{file_size}} {{file_type}})</span></a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
                 '</article>' +
+            '{{/.}}'+
+            '{{^.}}' +
+                '<p class="reports_empty">No Annual Reports available at this time</p>'+
             '{{/.}}'
         )
     },
